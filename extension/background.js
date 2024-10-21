42196e57714dd57e436535a8587a709b45fa194b
Pavel Gev <pavelgeveiler@gmail.com>
2024-10-21 11:17:45 +0200

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,19 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'makeHttpRequest') {
+        if (!/^https?:\/\//.test(request.url)) {
+            sendResponse({ success: false, error: "Invalid URL" });
+            return false;
+        }
+
         fetch(request.url, {
-            method: request.method,
-            headers: request.headers,
-            body: request.data
+            method: request.method || 'GET',
+            headers: request.headers || {},
+            body: request.method === 'POST' ? request.data : null
         })
-            .then(response => response.json())
-            .then(data => sendResponse({success: true, data: data}))
-            .catch(error => sendResponse({success: false, error: error.message}));
+        .then(response => response.json())
+        .then(data => sendResponse({success: true, data: data}))
+        .catch(error => sendResponse({success: false, error: error.message}));
+
         return true;
     }
 });

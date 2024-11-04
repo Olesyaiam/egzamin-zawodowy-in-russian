c076c7ffa18c268c871da7c699e283fdbb37af69
Pavel Gev <pavelgeveiler@gmail.com>
2024-11-05 00:29:07 +0100

    Update MenuController.php

--- server/app/Http/Controllers/MenuController.php
+++ server/app/Http/Controllers/MenuController.php
@@ -13,21 +13,21 @@ class MenuController extends BaseController
      */
     public function menu(Request $request): JsonResponse
     {
-        return $this->response([
-            'items' => [
-                // [
-                //     'text' => mb_strtoupper('Попробуйте такое же расширение для решения польских тестов ПДД на русском'),
-                //     'url' => 'https://chromewebstore.google.com/detail/gegcmhooaioaleibmojcgihkiepnobij',
-                //     'icon' => 'icon-info-sign',
-                //     'style' => 'color: red; font-size: 20px'
-                // ],
+        return $this->response(array(
+            'items' => array(
                 [
                     'text' => mb_strtoupper('Поставьте нам звезду на GitHub'),
                     'url' => 'https://github.com/Olesyaiam/egzamin-zawodowy-in-russian',
                     'icon' => 'icon-retweet',
                     'style' => 'color: green;'
                 ]
-            ]
-        ]);
+            ),
+            'remove' => array(
+                'mailto:kontakt@egzaminzawodowy.info',
+                'https://www.testy.egzaminzawodowy.info/wspolpraca.php',
+                'https://www.testy.egzaminzawodowy.info/nauka-zdalna.php',
+                'https://forum.egzaminzawodowy.info/'
+            )
+        ));
     }
 }

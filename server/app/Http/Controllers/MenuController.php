fad804f450a2449a95d4c542fff3a864cb81b205
Pavel Gev <pavelgeveiler@gmail.com>
2024-11-05 00:18:52 +0100

    Update MenuController.php

--- server/app/Http/Controllers/MenuController.php
+++ server/app/Http/Controllers/MenuController.php
@@ -15,12 +15,12 @@ class MenuController extends BaseController
     {
         return $this->response([
             'items' => [
-                [
-                    'text' => mb_strtoupper('Попробуйте такое же расширение для решения польских тестов ПДД на русском'),
-                    'url' => 'https://chromewebstore.google.com/detail/gegcmhooaioaleibmojcgihkiepnobij',
-                    'icon' => 'icon-info-sign',
-                    'style' => 'color: red; font-size: 20px'
-                ],
+                // [
+                //     'text' => mb_strtoupper('Попробуйте такое же расширение для решения польских тестов ПДД на русском'),
+                //     'url' => 'https://chromewebstore.google.com/detail/gegcmhooaioaleibmojcgihkiepnobij',
+                //     'icon' => 'icon-info-sign',
+                //     'style' => 'color: red; font-size: 20px'
+                // ],
                 [
                     'text' => mb_strtoupper('Поставьте нам звезду на GitHub'),
                     'url' => 'https://github.com/Olesyaiam/egzamin-zawodowy-in-russian',

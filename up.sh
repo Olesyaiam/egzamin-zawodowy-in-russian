c4783b180da533626fa329f3f01ba4d023504080
Pavel Gev <pavelgeveiler@gmail.com>
2025-10-22 22:31:48 +0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-sudo mkdir -p server/storage/logs server/storage/ramdisk_tmpfs
-sudo chown -R 33:33 server/storage
-sudo chmod -R u+rwX,g+rwX server/storage
+mkdir -p server/storage/logs server/storage/ramdisk_tmpfs
+chown -R 33:33 server/storage
+chmod -R u+rwX,g+rwX server/storage
 
 if docker compose version >/dev/null 2>&1; then
   COMPOSE=(docker compose)
